47b9515dfa64108dce7554e2e8160fe5dbdf2fa3
0x1 <kevin.francart3@gmail.com>
2026-08-23 13:16:40 +0200

    misc

--- src/Data/Foldable.rs
+++ src/Data/Foldable.rs
@@ -1,17 +1,17 @@
-pub fn Data_Foldable_foldrArray(f: std::rc::Rc<dyn Fn(crate::UnknownType) -> std::rc::Rc<dyn Fn(crate::UnknownType) -> crate::UnknownType>>, mut init: crate::UnknownType, mut xs: crate::UnknownType) -> crate::UnknownType {
+pub fn Data_Foldable_foldrArray(mut f: purust_core::Func2<crate::UnknownType, crate::UnknownType, crate::UnknownType>, mut init: crate::UnknownType, mut xs: crate::UnknownType) -> crate::UnknownType {
     let arr = xs.unwrap_array();
     let mut acc = init;
     for item in arr.iter().rev() {
-        acc = (f(item.clone()))(acc);
+        acc = f(item.clone(), acc);
     }
     acc
 }
 
-pub fn Data_Foldable_foldlArray(f: std::rc::Rc<dyn Fn(crate::UnknownType) -> std::rc::Rc<dyn Fn(crate::UnknownType) -> crate::UnknownType>>, mut init: crate::UnknownType, mut xs: crate::UnknownType) -> crate::UnknownType {
+pub fn Data_Foldable_foldlArray(mut f: purust_core::Func2<crate::UnknownType, crate::UnknownType, crate::UnknownType>, mut init: crate::UnknownType, mut xs: crate::UnknownType) -> crate::UnknownType {
     let arr = xs.unwrap_array();
     let mut acc = init;
     for item in arr.iter() {
-        acc = (f(acc))(item.clone());
+        acc = f(acc, item.clone());
     }
     acc
 }
